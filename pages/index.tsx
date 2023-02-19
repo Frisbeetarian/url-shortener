e2d6cb59337ca6e859549503593e27ad0bcb2ee7
Mohamad Sleiman Haidar <mhaidar@myki.com>
2023-02-19 15:38:49 +0300

    remove redundant div in index

--- pages/index.tsx
+++ pages/index.tsx
@@ -8,11 +8,10 @@ import {
   HStack,
   Stack,
 } from "@chakra-ui/react";
+
 import { Form, Formik } from "formik";
 import * as Yup from "yup";
 import { InputField } from "../components/InputField";
-
-// import { Form, Formik } from "formik";
 
 const ConvertUrl = Yup.object().shape({
   url: Yup.string()
@@ -22,57 +21,55 @@ const ConvertUrl = Yup.object().shape({
 });
 
 const Index = () => (
-  <div>
-    <Stack spacing={8} mx={"auto"} maxW={"lg"} py={12} px={6}>
-      <Stack align={"start"}>
-        <Heading fontSize={"4xl"} textAlign={"center"}>
-          URL Shortener
-        </Heading>
-      </Stack>
-
-      <Box boxShadow={"lg"} p={8} className="border bg-black">
-        <Formik
-          initialValues={{ url: "" }}
-          validationSchema={ConvertUrl}
-          onSubmit={async (values, {}) => {
-            console.log(values);
-          }}
-        >
-          {({ isSubmitting }) => (
-            <Form>
-              <Stack spacing={4}>
-                <HStack>
-                  <Box>
-                    <FormControl id="url" isRequired>
-                      <FormLabel>URL to shorten</FormLabel>
-
-                      <InputField name="url" placeholder="url" label="" />
-                    </FormControl>
-                  </Box>
-                </HStack>
-
-                <Stack spacing={10} pt={2}>
-                  <Button
-                    className="w-1/2 ml-auto "
-                    type="submit"
-                    size="md"
-                    bg={"green.400"}
-                    color={"white"}
-                    _hover={{
-                      bg: "green.900",
-                    }}
-                    isLoading={isSubmitting}
-                  >
-                    Submit
-                  </Button>
-                </Stack>
-              </Stack>
-            </Form>
-          )}
-        </Formik>
-      </Box>
+  <Stack spacing={8} mx={"auto"} maxW={"lg"} py={12} px={6} boxShadow="2xl">
+    <Stack align={"start"}>
+      <Heading fontSize={"4xl"} textAlign={"center"}>
+        URL Shortener
+      </Heading>
     </Stack>
-  </div>
+
+    <Box boxShadow={"lg"} p={8} className="border bg-black">
+      <Formik
+        initialValues={{ url: "" }}
+        validationSchema={ConvertUrl}
+        onSubmit={async (values, {}) => {
+          console.log(values);
+        }}
+      >
+        {({ isSubmitting }) => (
+          <Form>
+            <Stack spacing={4}>
+              <HStack>
+                <Box>
+                  <FormControl id="url" isRequired>
+                    <FormLabel>URL to shorten</FormLabel>
+
+                    <InputField name="url" placeholder="url" label="" />
+                  </FormControl>
+                </Box>
+              </HStack>
+
+              <Stack spacing={10} pt={2}>
+                <Button
+                  className="w-1/2 ml-auto"
+                  type="submit"
+                  size="md"
+                  bg={"green.400"}
+                  color={"white"}
+                  _hover={{
+                    bg: "green.900",
+                  }}
+                  isLoading={isSubmitting}
+                >
+                  Submit
+                </Button>
+              </Stack>
+            </Stack>
+          </Form>
+        )}
+      </Formik>
+    </Box>
+  </Stack>
 );
 
 export default Index;
